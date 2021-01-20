5b690ab8adcf9eaf96d7e8b300791084b5cc9bfe
sw1918 <spencer1918@live.com>
2021-01-19 16:57:27 -0800

    Add files via upload
    Adding controllers to fetch summary data for community and user snapshots, as well as specific users by ID

--- server/controllers/twitter-controller.js
+++ server/controllers/twitter-controller.js
@@ -25,3 +25,52 @@ const knex = require('knex')({
         res.json({ message: `There was an error retrieving data: ${err}` })
       })
   }
+
+ exports.userWhereID = async (req, res) => {
+    // Get all users from database
+    knex
+      .select('*') // select all records
+      .from('users') // from 'users' table
+      .where('id', req.params.id)
+      .then(userData => {
+        // Send books extracted from database in response
+        res.json(userData)
+      })
+      .catch(err => {
+        // Send a error message in response
+        res.json({ message: `There was an error retrieving data: ${err}` })
+      })
+  }
+
+  exports.communityAll = async (req, res) => {
+     // Get all users from database
+     knex
+       .select('*') // select all records
+       .from('community') // from 'users' table
+       .limit(100) // only the first 100 rows, for debugging
+       .then(userData => {
+         // Send books extracted from database in response
+         res.json(userData)
+       })
+       .catch(err => {
+         // Send a error message in response
+         res.json({ message: `There was an error retrieving data: ${err}` })
+       })
+   }
+
+   exports.snapshotsWhereID = async (req, res) => {
+    // Get all users from database
+    knex
+      .select('*') // select all records
+      .from('snapshots') // from 'users' table
+      .where('id', req.params.id)
+      .limit(100) // only the first 100 rows, for debugging
+      .then(userData => {
+        // Send books extracted from database in response
+        res.json(userData)
+      })
+      .catch(err => {
+        // Send a error message in response
+        res.json({ message: `There was an error retrieving data: ${err}` })
+      })
+  }
